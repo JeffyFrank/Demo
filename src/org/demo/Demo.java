0079b843d6666c80362211c7a2fd4d076c1ac7a4
JeffyFrank <jeffy.frank10@gmail.com>
2024-10-16 15:39:28 +0530

    Practice

--- src/org/demo/Demo.java
+++ src/org/demo/Demo.java
@@ -2,12 +2,10 @@ package org.demo;
 
 public class Demo {
 	public void demoOne() {
-	// TODO Auto-generated method stub
 System.out.println("Demo one done");
 }
 
 	public void demoTwo() {
-	// TODO Auto-generated method stub
 	 System.out.println("Demo two done");
 }
 }
