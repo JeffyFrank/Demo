4e5db2a675f0dc26c0d795df0e866ad9d9f5e84f
JeffyFrank <jeffy.frank10@gmail.com>
2024-10-16 16:21:59 +0530

    Adding vanmam

---
+++ src/org/podu/Vanmam.java
@@ -0,0 +1,14 @@
+package org.podu;
+
+public class Vanmam {
+	private static void vanmamVeri() {
+		// TODO Auto-generated method stub
+		System.out.println("Pakka plan");
+	
+	}
+	public static void main(String[] args) {
+		//Vanmam v = new Vanmam();
+		vanmamVeri();
+	}
+
+}
